e4f860762231a7929a2c640a9c2dea188f8cdb1c
Berger Peter <berger.peter@webdream.hu>
2022-11-30 09:34:59 +0100

    removed unused import

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Calculator from '../../Components/Calculator';
 import FORM from '../../Components/Form';
-import RESULT from '../../Components/Result';
 import HEADER from '../../Layouts/Header';
 
 const HOME = (props) => {
